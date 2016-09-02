bb42fe445e8aa2dbbbd90cfacf365c70ec041ae9
catiecook <catiecook888@gmail.com>
2016-09-02 10:52:38 -0600

    set up more things

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,8 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/heroku_express'
   },
 
   staging: {
